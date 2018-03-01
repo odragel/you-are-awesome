1858e38b21c283e07e49c6b08759cbaa20005b41
odragel <olga.dragel@gmail.com>
2018-03-01 23:31:30 +0300

    60

--- src/index.js
+++ src/index.js
@@ -5,39 +5,20 @@ const createNotEnumerableProperty = (propertyName) => {
     Object.defineProperty(Object.prototype, propertyName, ({enumerable: false, get: function(){ return value;}, set: function(newVal){value = newVal;}}));
     return propertyName};
 const createProtoMagicObject = () => { var a = new Function(); a.prototype = a.__proto__ ; return a;};
-const incrementor = () =>  {
-    if (typeof this.value == 'undefined'){
-        this.value = 1;
-        //    _value = 1;
-    } else{
-        this.value++;
-        //     _value = this.value;
-        //     _value++;
-    }
-
-    var _value = this.value;
-
-
-    function _incrementor() {
-
-        _value++;
-        this.value = _value;
-        return _incrementor;
-    };
-
-     Object.defineProperty(_incrementor, "valueOf", {value: function() { return _value; }});
-    Object.defineProperty(_incrementor, "toString", {value: function() { return _value.toString(); }});
-  //  _incrementor.valueOf = function () { return _value };
-
-    //   var i = _incrementor();
-    // console.log(_incrementor()() + 5);
-    return _incrementor;
-
-};
-
+const incrementor = () =>  {}
+;
 
 const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createIncrementer = () => {
+    var value = 0;
+    return {
+        next() {
+            return {value: ++value , done: false};
+        },
+        [Symbol.iterator]() { return this; }
+    }
+};
+
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = (argument) => {
