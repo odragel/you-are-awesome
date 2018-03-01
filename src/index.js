f5eca1a82c580ee3e3da8233cb166cf9a20af2c4
odragel <olga.dragel@gmail.com>
2018-03-01 15:19:28 +0300

    50

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
 // DO WHATEVER YOU WANT HERE
 
 const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+const createNotEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName, ({enumerable: false, get: function(){ return value;}, set: function(newVal){value = newVal;}}));
+    return propertyName};
+const createProtoMagicObject = () => { var a = new Function(); a.prototype = a.__proto__ ; return a;};
+const incrementor = () =>  {
+    if (typeof this.value == 'undefined'){
+        this.value = 1;
+        //    _value = 1;
+    } else{
+        this.value++;
+        //     _value = this.value;
+        //     _value++;
+    }
+
+    var _value = this.value;
+
+
+    function _incrementor() {
+
+        _value++;
+        this.value = _value;
+        return _incrementor;
+    };
+
+     Object.defineProperty(_incrementor, "valueOf", {value: function() { return _value; }});
+    Object.defineProperty(_incrementor, "toString", {value: function() { return _value.toString(); }});
+  //  _incrementor.valueOf = function () { return _value };
+
+    //   var i = _incrementor();
+    // console.log(_incrementor()() + 5);
+    return _incrementor;
+
+};
+
+
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (argument) => {
+    return new Promise(resolve => {
+    setTimeout(() => {
+    resolve(argument);
+}, 1000);
+});};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => (new Object(1));
 const toBuffer = () => {};
 const sortByProto = () => {};
 
